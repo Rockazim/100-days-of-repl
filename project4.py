40a0faed98267150f69c5072217faa78397a3786
Rockazim <122954720+AzimQudrat@users.noreply.github.com>
2024-11-14 10:22:21 -0800

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,13 @@
+print("Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with your dog as the star!")
+
+name = input("Whats your dogs name?")
+age = input("How old is your dog?")
+weight = input("How much does your dog weigh?")
+food = input("whats your dogs favorite food?")
+
+print("Once upon a time there was dog who was the king of kings he was the king of all kings to ever exist. He was the king of all kings because he was the king of all kings. This dog's name was", name, "and he was", age, "years old. He was the king of all kings because he was the king of all kings.")
+
+fear = input("what animal is your dog afraid of?")
+print("But one day his kingdom was threatened by a", "\033[31m", fear, ".\033[0m", "The dragon was the king of all", fear)
+
+print("Because of his fear he was banished from his kingdom and was never seen again. He began eating", food, "all day and then his weight was", weight, "because of this he never accomplished anything. So he stopped becoming the king of all kings." )
