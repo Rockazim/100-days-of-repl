e6181a40a83d24a448a1bf69b7e6a3f16e1f0573
Rockazim <122954720+AzimQudrat@users.noreply.github.com>
2024-11-14 09:54:22 -0800

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,8 @@
+food = input("Name a food >")
+plant = input("Name a type of plant >")
+cooking = input("Name a method of cooking >")
+description = input("What word describes burned food? >")
+diy_item = input("Name a DIY item >")
+
+print()
+print(cooking, food, "with", description, plant, "on a bed of", diy_item)
