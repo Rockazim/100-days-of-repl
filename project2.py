17af10694da918848e71c4d8ff25928bdc413c03
Rockazim <122954720+AzimQudrat@users.noreply.github.com>
2024-11-14 09:43:17 -0800

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,8 @@
+print("Getting to know you!")
+
+name = input("What's your name?: ")
+food = input("What's your favorite food?: ")
+music = input("What's your favorite music?: ")
+live = input("Where do you live?: ")
+
+print("You are\n", name, "You're probably hungry for\n", food, "and you're definitely getting your groove on to\n", music, "living in the amazing\n", live)
