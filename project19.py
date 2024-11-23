04c5e107d01e98dc4d1a71664f57353cdfee974f
Rockazim <122954720+AzimQudrat@users.noreply.github.com>
2024-11-23 08:22:26 -0800

    Add files via upload
    A common thing people do is borrow money. When people repay money, they pay interest which is paid back annually and added to the original amount the person borrowed.
    
    You are going to create a loan calculator that shows how much money you owe for a loan of $1,000 with a 5% APR (APR is fancy for Annual Percentage Rate) over 10 years.
    
    This means each year the amount of money you owe will increase 5%.
    
    Figure out how much you owe altogether for 10 years?
    
    Use a for loop and one or two lines of looped code to determine the answer. (Hint: Don't make this overcomplicated. It should only be a few lines of code altogether.)

---
+++ project19.py
@@ -0,0 +1,17 @@
+"""A common thing people do is borrow money. When people repay money, they pay interest which is paid back annually and added to the original amount the person borrowed.
+
+You are going to create a loan calculator that shows how much money you owe for a loan of $1,000 with a 5% APR (APR is fancy for Annual Percentage Rate) over 10 years.
+
+This means each year the amount of money you owe will increase 5%.
+
+Figure out how much you owe altogether for 10 years?
+
+Use a for loop and one or two lines of looped code to determine the answer. (Hint: Don't make this overcomplicated. It should only be a few lines of code altogether.)"""
+loan = 1000
+
+for i in range(10):
+  loan += loan * 0.05
+  print("Year", i+1, "is", loan)
+
+
+print("\nYou paid", loan - 1000, "in interest!")
